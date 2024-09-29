e9d97a01cc6b6a242c1c2a8782cc70b5fd6e6d1d
Jhomar 'Joe' Nando <contato@joekyy.com.br>
2024-09-28 21:26:42 -0300

    feat: add responsive mobile menu with hamburger btn and other areas

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -3,7 +3,6 @@ function toggleDropdown() {
     dropdown.classList.toggle('hidden');
 }
 
-// Close dropdown if clicked outside
 window.onclick = function(event) {
     if (!event.target.matches('#dropdownButton')) {
         var dropdown = document.getElementById("dropdownMenu");
@@ -19,11 +18,56 @@ $('.sliderHome').slick({
     arrows: true,
     responsive: [
         {
-          breakpoint: 1024,
-          settings: {
+            breakpoint: 1024,
+            settings: {
             arrows: false,
             slidesToShow: 1
-          }
+            }
         },
-      ]
-  });
+    ]
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const words = ["gerencia", "otimiza", "analisa", "pivota", "domina"];
+    let currentWordIndex = 0;
+    let currentCharIndex = 0;
+    let isDeleting = false;
+    const typingSpeed = 150;
+    const deletingSpeed = 100;
+    const delayBetweenWords = 2000;
+    const targetElement = document.querySelector("#text-animation");
+
+    function updateText() {
+        const currentWord = words[currentWordIndex];
+        if (isDeleting) {
+            targetElement.textContent = currentWord.substring(0, currentCharIndex--);
+            if (currentCharIndex < 0) {
+                isDeleting = false;
+                currentWordIndex = (currentWordIndex + 1) % words.length;
+                setTimeout(() => requestAnimationFrame(updateText), typingSpeed);
+                return;
+            }
+        } else {
+            targetElement.textContent = currentWord.substring(0, currentCharIndex++);
+            if (currentCharIndex > currentWord.length) {
+                isDeleting = true;
+                setTimeout(() => requestAnimationFrame(updateText), delayBetweenWords);
+                return;
+            }
+        }
+        const speed = isDeleting ? deletingSpeed : typingSpeed;
+        setTimeout(() => requestAnimationFrame(updateText), speed);
+    }
+
+    requestAnimationFrame(updateText);
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const menuButton = document.getElementById('menuButton');
+    const mobileMenu = document.getElementById('mobileMenu');
+
+    menuButton.addEventListener('click', function () {
+        mobileMenu.classList.toggle('hidden');
+    });
+});
+
